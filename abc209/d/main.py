243883db2525556d1020ffe64acbd89c9c9089e0
Shuhei Abe <blueletter123456789@gmail.com>
2022-11-26 19:01:09 +0900

    modify ABC209/d

--- abc209/d/main.py
+++ abc209/d/main.py
@@ -1,28 +1,3 @@
-# from collections import deque
-
-# n, q = map(int, input().split())
-
-# G = [[] for _ in range(n)]
-# for _ in range(n):
-#     a, b = map(int, input().split())
-#     G[a-1].append(b-1)
-#     G[b-1].append(a-1)
-
-# dist = [[-1, float('inf')] for _ in range(n)]
-# dist[0][1] = 0
-# que = deque([0])
-# while len(que):
-#     current = que.popleft()
-#     _, cur_dist = dist[current]
-    
-#     for e in G[current]:
-#         if dist[e][1] > cur_dist + 1:
-#             dist[e] = [current, cur_dist + 1]
-#             que.append(e)
-
-# for i in range(q):
-#     c, d = map(int, input().split())
-
 from collections import deque
 
 n, q = map(int, input().split())
