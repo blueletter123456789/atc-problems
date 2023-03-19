281a483253cd5be50f6f5e3f9da8e2939e90b681
Shuhei Abe <blueletter123456789@gmail.com>
2023-03-19 10:14:44 +0900

    テスト用に関数化

--- arc137/a/main.py
+++ arc137/a/main.py
@@ -2,14 +2,18 @@ def gcd(a, b):
     return a if b == 0 else gcd(b, a % b)
 
 
-left, right = map(int, input().split())
-max_num = 0
-max_idx = min(1500, right - left)
+def main(left, right):
+    max_num = 0
+    max_idx = min(1500, right - left)
 
-for i in range(max_idx):
-    tmp_l, tmp_r = left, right - i
-    while gcd(tmp_r, tmp_l) != 1:
-        tmp_l += 1
-    max_num = max(max_num, tmp_r - tmp_l)
+    for i in range(max_idx):
+        tmp_l, tmp_r = left, right - i
+        while gcd(tmp_r, tmp_l) != 1:
+            tmp_l += 1
+        max_num = max(max_num, tmp_r - tmp_l)
+    return max_num
 
-print(max_num)
+
+if __name__ == "__main__":
+    left, right = map(int, input().split())
+    print(main(left, right))
